ad892076b471967b96f3e152da1ab749fa0e5183
Tobi Balogun <tobi.balo@gmail.com>
2022-12-18 09:27:37 +0100

    improve: add MongoServerError messages

--- src/middleware/errorHandler.js
+++ src/middleware/errorHandler.js
@@ -1,11 +1,15 @@
 function errorHandler(error, req, res, next) {
   console.log('🚀 ~ file: errorHandler.js:2 ~ error', error)
 
+  if (error.name === 'MongoServerError' && error.code === 11000) {
+    return next(res.status(400).send(`${Object.keys(error.keyValue)[0]} must be unique`))
+  }
+
   if (error.name === 'ValidationError') {
     return next(res.status(400).send(error.details[0].message))
   }
 
-  res.send(error.status || 500, 'Something broke')
+  res.status(error.status || 500).send('Something broke')
   next()
 }
 
